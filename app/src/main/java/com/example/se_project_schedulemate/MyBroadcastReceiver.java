8fa6f598e4ebfd21e1b18d0a57943199d2074f85
alcemyth <david99aja12@gmail.com>
2023-06-11 01:16:19 +0700

    setAlarm

---
+++ app/src/main/java/com/example/se_project_schedulemate/MyBroadcastReceiver.java
@@ -0,0 +1,63 @@
+package com.example.se_project_schedulemate;
+
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.Vibrator;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+public class MyBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        Intent i = new Intent(context, SetAlarm.class);
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, i, PendingIntent.FLAG_MUTABLE);
+
+        Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        vibrator.vibrate(2000);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "Alarm Reminders")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentText("Alarm")
+                .setContentText("Hey, Wake Up!")
+                .setAutoCancel(true)
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(pendingIntent);
+
+
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context);
+        if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        notificationManagerCompat.notify(200, builder.build());
+
+        Uri sound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
+
+        Ringtone r = RingtoneManager.getRingtone(context,sound);
+        r.play();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                r.stop();
+            }
+        }, 60000); // 60 seconds delay
+    }
+}
---
+++ app/src/main/java/com/example/se_project_schedulemate/SetAlarm.java
@@ -0,0 +1,89 @@
+package com.example.se_project_schedulemate;
+
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class SetAlarm extends AppCompatActivity {
+    private TimePicker timePicker;
+    private Button saveAlarm;
+//    private EditText edit = (EditText) findViewById(R.id.alarmName);
+//    public static String alarmName;
+    private int hour, minutes;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.set_alarm_activity);
+
+        timePicker = findViewById(R.id.timePicker);
+        saveAlarm = findViewById(R.id.saveAlarm);
+//        alarmName = (String) edit.getText().toString();
+        timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+                hour = hourOfDay;
+                minutes = minute;
+            }
+        });
+
+        saveAlarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SetAlarm.this, "Set Alarm " + hour + ": "+ minutes, Toast.LENGTH_SHORT).show();
+                setTimer();
+                notification();
+            }
+        });
+    }
+
+    private void notification() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            CharSequence name = "Alarm";
+            String description = "Hey, Wake Up!!";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+
+            NotificationChannel channel = new NotificationChannel("Alarm Reminders", name, NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    private void setTimer() {
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        Date date = new Date();
+
+        Calendar cal_alarm = Calendar.getInstance();
+        Calendar cal_now = Calendar.getInstance();
+
+        cal_now.setTime(date);
+        cal_alarm.setTime(date);
+
+        cal_alarm.set(Calendar.HOUR_OF_DAY, hour);
+        cal_alarm.set(Calendar.MINUTE, minutes);
+        cal_alarm.set(Calendar.SECOND, 0);
+
+        if(cal_alarm.before((cal_now))){
+            cal_alarm.add(Calendar.DATE, 1);
+        }
+
+        Intent i = new Intent(SetAlarm.this, MyBroadcastReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(SetAlarm.this, 0, i, PendingIntent.FLAG_IMMUTABLE);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, cal_alarm.getTimeInMillis(),pendingIntent);
+    }
+}
