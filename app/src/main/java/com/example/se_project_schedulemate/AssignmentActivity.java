01ac5f7604b397a52c3ee45e9907687cd95bea81
nt <nicholast.theodore@binus.ac.id>
2023-06-11 20:08:16 +0700

    Assignment assignment pagenya 50%

--- app/src/main/java/com/example/se_project_schedulemate/AssignmentActivity.java
+++ app/src/main/java/com/example/se_project_schedulemate/AssignmentActivity.java
@@ -1,12 +1,17 @@
 package com.example.se_project_schedulemate;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.sql.Timestamp;
 import java.util.Vector;
@@ -18,17 +23,17 @@ public class AssignmentActivity extends AppCompatActivity implements MyInterface
     public void init(){
         rv_Assignment = findViewById(R.id.rv_Assignment);
         assignmentVector = new Vector<>();
-        assignmentVector.add(new Assignment("Asg",
+        assignmentVector.add(new Assignment("Asg1",
                 new Timestamp(22,12,22,7,0,0,0), 7,
-                "Dosen",
+                "Dosen1",
                 new Timestamp(21,12,22,7,0,0,0)));
-        assignmentVector.add(new Assignment("Asg",
+        assignmentVector.add(new Assignment("Asg2",
                 new Timestamp(20,12,22,7,0,0,0), 7,
-                "Dosen",
+                "Dosen 2",
                 new Timestamp(24,12,22,7,0,0,0)));
-        assignmentVector.add(new Assignment("Asg",
+        assignmentVector.add(new Assignment("Asg3",
                 new Timestamp(20,12,22,7,0,0,0), 7,
-                "Dosen",
+                "Dosen 3",
                 new Timestamp(24,12,22,7,0,0,0)));
 
         AssignmentAdapter adapter = new AssignmentAdapter(this, this);
@@ -38,12 +43,52 @@ public class AssignmentActivity extends AppCompatActivity implements MyInterface
         rv_Assignment.setLayoutManager(new LinearLayoutManager(this));
     }
 
+    public void viewAsgDetails(int position) {
+        String assignmentTitle = assignmentVector.get(position).getAssignmentName();
+        Integer deadlineYear = assignmentVector.get(position).getDeadline().getYear();
+        Integer deadlineMonth = assignmentVector.get(position).getDeadline().getMonth();
+        Integer deadlineDay = assignmentVector.get(position).getDeadline().getDate();
+        Integer deadlineHour = assignmentVector.get(position).getDeadline().getHours();
+        Integer deadlineMinute = assignmentVector.get(position).getDeadline().getMinutes();
+
+
+        Intent intentAsgDetail = new Intent(this, AssignmentDetail.class);
+        intentAsgDetail.putExtra("Title", assignmentTitle);
+        intentAsgDetail.putExtra("Deadline Year", deadlineYear);
+        intentAsgDetail.putExtra("Deadline Month", deadlineMonth);
+        intentAsgDetail.putExtra("Deadline Day", deadlineDay);
+        intentAsgDetail.putExtra("Deadline Hour", deadlineHour);
+        intentAsgDetail.putExtra("Deadline Minute", deadlineMinute);
+        startActivity(intentAsgDetail);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assignment);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNavigationView);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
         init();
     }
+
+    BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener(){
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    if(item.getItemId() == R.id.alarms_menu){
+                        Intent intent = new Intent(AssignmentActivity.this, AlarmsPageActivity.class);
+                        startActivity(intent);
+                    }
+//                    else if (item.getItemId() == R.id.forums_menu) {
+//                        Intent intent = new Intent(AssignmentActivity.this, Forum.class);
+//                        startActivity(intent);
+//                    }
+
+                    return true;
+                }
+            };
 
     @Override
     public void onClick(int position) {
---
+++ app/src/main/java/com/example/se_project_schedulemate/AssignmentDetail.java
@@ -0,0 +1,14 @@
+package com.example.se_project_schedulemate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AssignmentDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assignment_detail);
+    }
+}
