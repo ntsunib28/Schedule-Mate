5f9e801069e30d27db15c6b037604ae5750cdb70
nt <nicholast.theodore@binus.ac.id>
2023-06-11 14:50:33 +0700

    Test

--- app/src/main/java/com/example/se_project_schedulemate/AssignmentActivity.java
+++ app/src/main/java/com/example/se_project_schedulemate/AssignmentActivity.java
@@ -2,6 +2,7 @@ package com.example.se_project_schedulemate;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -18,19 +19,23 @@ public class AssignmentActivity extends AppCompatActivity implements MyInterface
         rv_Assignment = findViewById(R.id.rv_Assignment);
         assignmentVector = new Vector<>();
         assignmentVector.add(new Assignment("Asg",
-                new Timestamp(2023,12,22,7,0,0,0), 7,
+                new Timestamp(22,12,22,7,0,0,0), 7,
                 "Dosen",
-                new Timestamp(2023,12,22,7,0,0,0)));
+                new Timestamp(21,12,22,7,0,0,0)));
         assignmentVector.add(new Assignment("Asg",
-                new Timestamp(2023,12,22,7,0,0,0), 7,
+                new Timestamp(20,12,22,7,0,0,0), 7,
                 "Dosen",
-                new Timestamp(2023,12,22,7,0,0,0)));
+                new Timestamp(24,12,22,7,0,0,0)));
+        assignmentVector.add(new Assignment("Asg",
+                new Timestamp(20,12,22,7,0,0,0), 7,
+                "Dosen",
+                new Timestamp(24,12,22,7,0,0,0)));
 
         AssignmentAdapter adapter = new AssignmentAdapter(this, this);
         adapter.setAssignments(assignmentVector);
 
         rv_Assignment.setAdapter(adapter);
-        rv_Assignment.setLayoutManager(new GridLayoutManager(this, 2));
+        rv_Assignment.setLayoutManager(new LinearLayoutManager(this));
     }
 
     @Override
