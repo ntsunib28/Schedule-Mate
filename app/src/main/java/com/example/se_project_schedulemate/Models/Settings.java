beac9714a28246e24b3b1d10fa967f762fbc645f
nt <nicholast.theodore@binus.ac.id>
2023-06-18 23:25:00 +0700

    Setting udah nampilin setting usernya

---
+++ app/src/main/java/com/example/se_project_schedulemate/Models/Settings.java
@@ -0,0 +1,32 @@
+package com.example.se_project_schedulemate.Models;
+
+public class Settings {
+    int assignment_reminder_days;
+
+    public int getAssignment_reminder_days() {
+        return assignment_reminder_days;
+    }
+
+    public void setAssignment_reminder_days(int assignment_reminder_days) {
+        this.assignment_reminder_days = assignment_reminder_days;
+    }
+
+    public int getClass_alarm_minutes() {
+        return class_alarm_minutes;
+    }
+
+    public void setClass_alarm_minutes(int class_alarm_minutes) {
+        this.class_alarm_minutes = class_alarm_minutes;
+    }
+
+    public int getForum_reminder_days() {
+        return forum_reminder_days;
+    }
+
+    public void setForum_reminder_days(int forum_reminder_days) {
+        this.forum_reminder_days = forum_reminder_days;
+    }
+
+    int class_alarm_minutes;
+    int forum_reminder_days;
+}
--- app/src/main/java/com/example/se_project_schedulemate/SettingsActivity.java
+++ app/src/main/java/com/example/se_project_schedulemate/SettingsActivity.java
@@ -1,5 +1,6 @@
 package com.example.se_project_schedulemate;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
@@ -14,19 +15,28 @@ import android.widget.NumberPicker;
 import android.widget.TextView;
 
 import com.example.se_project_schedulemate.Login.Login;
+import com.example.se_project_schedulemate.Models.Settings;
+import com.example.se_project_schedulemate.Models.User;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SettingsActivity extends AppCompatActivity {
 
     FirebaseAuth auth;
-
-
     FirebaseUser user;
+    private DatabaseReference mReference;
+    final FirebaseDatabase mDatabase = FirebaseDatabase.getInstance("https://schedule-mate-70c31-default-rtdb.asia-southeast1.firebasedatabase.app/");
+
     CardView logoutBtn, applySettings;
     ImageView backBtn;
     NumberPicker npClassSettings, npARSettings, npForumSettings;
-    TextView descClass;
+    TextView descClass, descAsg2, descForum2;
+    int userClassSetting, userAsgSetting, userForumSetting;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,42 +51,61 @@ public class SettingsActivity extends AppCompatActivity {
             finish();
         }
 
+        mReference = mDatabase.getReference().child("user_settings").child(auth.getUid());
+        mReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Settings settingTemp = snapshot.getValue(Settings.class);
+                npClassSettings.setValue(settingTemp.getClass_alarm_minutes());
+                npARSettings.setValue(settingTemp.getAssignment_reminder_days());
+                npForumSettings.setValue(settingTemp.getForum_reminder_days());
+
+                descClass.setText(settingTemp.getClass_alarm_minutes() + " Minutes Before");
+                descAsg2.setText(settingTemp.getAssignment_reminder_days() + " Days");
+                descForum2.setText(settingTemp.getForum_reminder_days() + " Days");
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
 
 //        NUMBER PICKER
         npClassSettings = findViewById(R.id.npClassSettings);
         descClass = findViewById(R.id.descClass);
         npClassSettings.setMinValue(1);
         npClassSettings.setMaxValue(60);
-        npClassSettings.setValue(30);
-//      npClassSettings.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
-//            @Override
-//            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-//                Log.d("TES NP", "onValueChange: ");
-//                descClass.setText(newVal + "Minutes Before");
-//            }
-//        });
+      npClassSettings.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                descClass.setText(newVal + " Minutes Before");
+            }
+        });
 
         npARSettings = findViewById(R.id.npARSettings);
+        descAsg2 = findViewById(R.id.descAsg2);
         npARSettings.setMinValue(1);
         npARSettings.setMaxValue(30);
-        npARSettings.setValue(3);
-//      npARSettings.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
-//            @Override
-//            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-//
-//            }
-//        });
+      npARSettings.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                descAsg2.setText(newVal + " Days");
+            }
+        });
 
         npForumSettings = findViewById(R.id.npForumSettings);
+        descForum2 = findViewById(R.id.descForum2);
         npForumSettings.setMinValue(1);
         npForumSettings.setMaxValue(30);
-        npForumSettings.setValue(3);
-//        npForumSettings.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
-//            @Override
-//            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-//
-//            }
-//        });
+        npForumSettings.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                descForum2.setText(newVal + " Days");
+            }
+        });
 
 
 
